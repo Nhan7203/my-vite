878af858275ebe47766a4a3b098dcd56eee925ba
Thanh Nhan <nhanntse172412@fpt.edu.vn>
2024-05-21 16:54:14 +0700

    Begin

--- src/pages/Cart-page/Cart.tsx
+++ src/pages/Cart-page/Cart.tsx
@@ -55,11 +55,31 @@ const ShoppingCart = () => {
             ))}
           </div>
           <div className="box-right">
-            {/* ... */}
+          <div className="adress">
+              <h4>Shipping Address</h4>
+              <div className="box-adress">
+                <div>Confirm Shipping Address</div>
+              </div>
+            </div>
+            <div className="voucher">
+              <h4>Voucher</h4>
+              <div className="box-voucher">
+                <div>Promo Code</div>
+              </div>
+            </div>
+            <div className="bill">
+              <div>Subtotal</div>
+              <div className="money-voucher">Voucher</div>
+              <div className="total">Total</div>
+              <div className="vat">(Incl. VAT)</div>
+              <div className="box-continue">Continue</div>
+            </div>
+          </div>
+
           </div>
         </div>
       </div>
-    </div>
+   
   );
 };
 
