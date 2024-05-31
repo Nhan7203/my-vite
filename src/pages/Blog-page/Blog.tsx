812d8e66d785532edce6f726bd9366f1ba1e42ae
Thanh Nhan <nhanntse172412@fpt.edu.vn>
2024-05-31 19:38:20 +0700

    Created blog page

--- src/pages/Blog-page/Blog.tsx
+++ src/pages/Blog-page/Blog.tsx
@@ -1,9 +1,60 @@
-const Blog = () => {
-  return (
-    <>
-      <div>Blog</div>
-    </>
-  );
-};
+// import { Navbar, Footer } from "../../import/import-router";
+// import { useState, useEffect  } from "react";
+// import "./Blog.css";
 
-export default Blog;
+
+// export interface Blog {
+//   blogId: number;
+//   title: string;
+//   description: string;
+//   imageBlog: string;
+// }
+
+// const getGridColumn = (index: number) => {
+//   const gridColumnMap = ["1 / 4", "5 / 8", "9 / 12"];
+//   return gridColumnMap[index % gridColumnMap.length];
+// };
+
+// const Blog = () => {
+
+//   const [blogList, setBlogList] = useState<Blog[]>([]);
+
+// //Get API blog
+//   useEffect(() => {
+//     const fetchData = async () => {
+//       const result = ;
+//       setBlogList(result);
+//     };
+//     fetchData();
+//   }, []);
+
+//   return (
+//     <div>
+//       <Navbar />
+
+//       <div className="body-blog">
+//         <div>
+//           {blogList.map((blog, index) => (
+//             <div
+//               className="box-blog"
+//               style={{
+//                 gridColumn: getGridColumn(index),
+//               }}
+//             >
+//               <div className="element-blog">
+//                 <img src={} className="img-card" alt="" />
+//                 <div>
+//                   <p>icon View</p>
+//                   <p>MM/DD/YY</p>
+//                 </div>
+//               </div>
+//             </div>
+//           ))}
+//         </div>
+//       </div>
+//       <Footer />
+//     </div>
+//   );
+// };
+
+// export default Blog;
